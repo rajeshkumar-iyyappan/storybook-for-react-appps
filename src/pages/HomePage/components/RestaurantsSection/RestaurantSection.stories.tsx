c99540643098427a63e60b7b6f2da25fb1871e46
rajeshkumar_iyyappan <rajeshkumar.iyyappan@gmail.com>
2023-01-09 00:15:18 +0530

    Api request -  modified component level

--- src/pages/HomePage/components/RestaurantsSection/RestaurantSection.stories.tsx
+++ src/pages/HomePage/components/RestaurantsSection/RestaurantSection.stories.tsx
@@ -1,12 +1,10 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
-import { restaurants } from '../../../../stub/restaurants'
-
-import { RestaurantsSectionComponent as RestaurantSection } from './RestaurantsSection.container'
+import { RestaurantsSection } from './RestaurantsSection'
 
 export default {
   title: 'Pages/Homepage/RestaurantsSection',
-  component: RestaurantSection,
+  component: RestaurantsSection,
   args: {
     title: 'Our favourite picks',
   },
@@ -16,18 +14,11 @@ export default {
       url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=1126%3A3893&t=E6fUzobJdMiCA09Q-4',
     },
   },
-} as ComponentMeta<typeof RestaurantSection>
+} as ComponentMeta<typeof RestaurantsSection>
 
-const Template: ComponentStory<typeof RestaurantSection> = (args: any) => (
-  <RestaurantSection {...args} />
+const Template: ComponentStory<typeof RestaurantsSection> = (args: any) => (
+  <RestaurantsSection {...args} />
 )
 
 export const Default = Template.bind({})
-Default.args = {
-  restaurants,
-}
-
-export const Loading = Template.bind({})
-Loading.args = {
-  isLoading: true,
-}
+Default.args = {}
