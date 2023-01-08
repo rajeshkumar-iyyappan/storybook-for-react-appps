264f5f0cd59e4f907186d922270c5b6bc095a381
rajeshkumar_iyyappan <rajeshkumar.iyyappan@gmail.com>
2023-01-09 00:08:08 +0530

    Container pattern changes

--- src/pages/HomePage/components/RestaurantsSection/RestaurantSection.stories.tsx
+++ src/pages/HomePage/components/RestaurantsSection/RestaurantSection.stories.tsx
@@ -1,23 +1,33 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 
-import { RestaurantsSection } from './RestaurantsSection.container'
+import { restaurants } from '../../../../stub/restaurants'
+
+import { RestaurantsSectionComponent as RestaurantSection } from './RestaurantsSection.container'
 
 export default {
   title: 'Pages/Homepage/RestaurantsSection',
-  component: RestaurantsSection,
+  component: RestaurantSection,
+  args: {
+    title: 'Our favourite picks',
+  },
   parameters: {
     design: {
       title: 'figspec',
       url: 'https://www.figma.com/file/3Q1HTCalD0lJnNvcMoEw1x/Mealdrop?node-id=1126%3A3893&t=E6fUzobJdMiCA09Q-4',
     },
   },
-} as ComponentMeta<typeof RestaurantsSection>
+} as ComponentMeta<typeof RestaurantSection>
 
-const Template: ComponentStory<typeof RestaurantsSection> = (args: any) => (
-  <RestaurantsSection {...args} />
+const Template: ComponentStory<typeof RestaurantSection> = (args: any) => (
+  <RestaurantSection {...args} />
 )
 
 export const Default = Template.bind({})
 Default.args = {
-  title: 'Our favourite picks',
+  restaurants,
+}
+
+export const Loading = Template.bind({})
+Loading.args = {
+  isLoading: true,
 }
